6a601047b7d1ea647dc7848226ab9c7ea567086a
johnmwangi <jonesmwas356@gmail.com>
2019-03-25 11:50:51 +0300

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+// import { QuoteDetailsComponent } from './quote-details/goal-details.component';
+// import {  StrikeThroughDirective  } from './strike.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { StrikeThroughDirective } from './strike-through.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    StrikeThroughDirective,
+    DateCountPipe,
+    QuoteFormComponent,
+    QuoteDetailsComponent,
+    StrikeThroughDirective
   ],
   imports: [
     BrowserModule,
